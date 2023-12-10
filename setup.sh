45da4c8108592bac8056c18114f048a127e5d2bd
Caden Shmookler <cshmookler@gmail.com>
2023-12-10 12:08:57 -0700

    Make neovim source directory

--- setup.sh
+++ setup.sh
@@ -344,6 +344,7 @@ if [[ "'$SETUP_DEVELOPMENT_TOOLS'" = "true" ]]; then
     echo "----------------------------------------"
     echo "Installing neovim..."
     SETUP_NVIM_SOURCE=/etc/nvim_source
+    mkdir -p $SETUP_NVIM_SOURCE || quit "Failed to create neovim source directory"
     curl -L https://github.com/neovim/neovim/archive/refs/tags/nightly.tar.gz -o $SETUP_NVIM_SOURCE/nightly.tar.gz || quit "Failed to download neovim"
     tar -xf nightly.tar.gz || quit "Failed to extract neovim"
     cd $SETUP_NVIM_SOURCE/neovim-nightly || quit "Failed to change directory to $SETUP_NVIM_SOURCE/neovim-nightly"
